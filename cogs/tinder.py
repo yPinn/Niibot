c32c7be188ebfe47054bb4a9877e255853fe1a89
yPinn <david010556@gmail.com>
2025-05-08 17:57:58 +0800

    tinder

--- cogs/tinder.py
+++ cogs/tinder.py
@@ -1,18 +1,36 @@
 import discord
 from discord.ext import commands
+import json
+import os
+
+DATA_FILE = 'data/tinder.json'
 
 
 class Tinder(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.user_data = self.load_data()
+
+    def load_data(self):
+        if os.path.exists(DATA_FILE):
+            with open(DATA_FILE, "r") as f:
+                return json.load(f)
+
+        return {}
+
+    def save_data(self):
+        with open(DATA_FILE, "w") as f:
+            json.dump(self.user_data, f, indent=4)
 
     @commands.command()
-    async def home(self, ctx: commands.Context):
-
-
-
+    async def status(self, ctx: commands.Context):
+        uid = str(ctx.author.id)
+        if uid in self.user_data:
+            data = self.user_data[uid]
+            await ctx.send()
 
     # 前綴指令
+
     @commands.command()
     async def tinder(self, ctx: commands.Context):
         await ctx.send("MATCH!!!")
