308604fb7900257ddf143282927bb13f14249cb7
yPinn <140962499+yPinn@users.noreply.github.com>
2025-05-21 02:10:26 +0800

    embed可顯示

--- bot.py
+++ bot.py
@@ -1,16 +1,17 @@
-import os
 import asyncio
+import os
+
 import discord
 from discord.ext import commands
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="?", intents=intents)
 
 
 @bot.event
 async def on_ready():
     print(f"目前登入身份 --> {bot.user}")
-    game = discord.Game("$help")
+    game = discord.Game("?help")
     await bot.change_presence(status=discord.Status.online, activity=game)
 
 
@@ -41,7 +42,10 @@ async def load_extensions():
 async def main():
     async with bot:
         await load_extensions()
-        await bot.start("MTM2NDEwMzgxODMyMDgwNTkxOA.GfuAe_.fE6_EXgaIm663NsMwRna8_QvB-yivw02TOJ3zI")
+        await bot.start(
+            "MTM2NDEwMzgxODMyMDgwNTkxOA.GfuAe_.fE6_EXgaIm663NsMwRna8_QvB-yivw02TOJ3zI"
+        )
+
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ cogs/clear.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+
+
+class Clear(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="clearall")
+    @commands.has_permissions(manage_messages=True)
+    async def clear_all_messages(self, ctx: commands.Context):
+        deleted = await ctx.channel.purge(limit=None)
+        await ctx.send(f"✅ 已清除 {len(deleted)} 則訊息", delete_after=3)
+
+    @clear_all_messages.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("❌ 你沒有權限使用這個指令！")
+        else:
+            await ctx.send(f"⚠️ 發生錯誤：{str(error)}")
+
+
+async def setup(bot):
+    await bot.add_cog(Clear(bot))
