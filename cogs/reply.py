c526241da8a1afb67fefe340871fe9a34b7edf5e
yPinn <david010556@gmail.com>
2025-06-01 00:12:21 +0800

    25/06/01: rename(reply)

--- cogs/reply.py
+++ cogs/reply.py
@@ -1,3 +1,5 @@
+import datetime
+import os
 import random
 
 import discord
@@ -20,14 +22,20 @@ class Reply(commands.Cog):
         mentioned_user_ids = [user.id for user in message.mentions]
 
         msg = [
-            "不要 @ 我，幹你娘！！！", "不熟N標", "?",
-            "幹你娘機掰標三小", "皮 ↘ 炎 ↗", f"不用 標<@{target_user_id}>就好", "uu：愛是寂寞人"
+            "不要 @ 我，幹你娘！！！",
+            "不熟N標",
+            "?",
+            "幹你娘機掰標三小",
+            "皮 ↘ 炎 ↗",
+            f"不用標 標<@{target_user_id}>就好",
+            "uu：愛是寂寞人",
+            "不要再冒充我的身分了",
         ]
 
         if target_role_id in mentioned_role_ids or target_user_id in mentioned_user_ids:
             await message.reply(random.choice(msg), mention_author=True)
 
-    @commands.command(name="cc", aliases=["複製"], help="複製人，顯示頭像和橫幅")
+    @commands.command(name="cc", aliases=["複製", "ditto"], help="複製人，顯示頭像和橫幅")
     async def copycat(self, ctx: commands.Context, *, user_input: str):
         user = None
 
@@ -37,7 +45,7 @@ class Reply(commands.Cog):
             if ctx.guild:
                 user = discord.utils.find(
                     lambda m: m.name == user_input or m.display_name == user_input,
-                    ctx.guild.members
+                    ctx.guild.members,
                 )
             if not user:
                 try:
@@ -46,8 +54,12 @@ class Reply(commands.Cog):
                     if user is None:
                         user = await self.bot.fetch_user(user_id)
                 except ValueError:
-                    await ctx.send(embed=discord.Embed(
-                        description="⚠️ 請提供有效的用戶 ID、名稱或 @提及", color=discord.Color.red()))
+                    await ctx.send(
+                        embed=discord.Embed(
+                            description="⚠️ 請提供有效的用戶 ID、名稱或 @提及",
+                            color=discord.Color.red(),
+                        )
+                    )
                     return
                 except discord.NotFound:
                     await ctx.send("⚠️ 找不到該用戶")
@@ -72,8 +84,12 @@ class Reply(commands.Cog):
 
         embed = discord.Embed(
             title=f"{user.display_name} 的資料",
-            color=embed_color
+            color=embed_color,
+            timestamp=datetime.datetime.now(),
         )
+
+        embed.set_author(
+            name="Ditto", icon_url="https://i.pinimg.com/736x/41/0b/a5/410ba54a0c7ca00f359d008f4fcebcd0.jpg")
         embed.set_thumbnail(url=avatar_url)
         embed.add_field(name="頭像", value=f"[點我查看]({avatar_url})", inline=False)
 
@@ -86,6 +102,8 @@ class Reply(commands.Cog):
 
         if accent_color:
             embed.add_field(name="主題顏色", value=str(accent_color), inline=False)
+
+        embed.set_footer(text="Niibot", icon_url=self.bot.user.avatar.url)
 
         await ctx.send(embed=embed)
 
