7326b29218870f31373d27256feac4b27cc56780
yPinn <david010556@gmail.com>
2026-04-13 11:50:59 +0800

    refactor(frontend): migrate Tabs to @radix-ui/react-tabs
    - Replace radix-ui unified package with @radix-ui/react-tabs to match
      the rest of the UI components (was the only file using the unified pkg)
    - Bump frontend version to 1.0.0

--- frontend/src/components/ui/tabs.tsx
+++ frontend/src/components/ui/tabs.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
+import * as TabsPrimitive from '@radix-ui/react-tabs'
 import { cva, type VariantProps } from 'class-variance-authority'
-import { Tabs as TabsPrimitive } from 'radix-ui'
 
 import { cn } from '@/lib/utils'
 
