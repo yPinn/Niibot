7caec753b371f53c7f4b7101f40af476420109c4
yPinn <david010556@gmail.com>
2026-04-28 01:39:21 +0800

    chore: remove unused analytics dataclasses

--- backend/shared/models/__init__.py
+++ backend/shared/models/__init__.py
@@ -1,6 +1,5 @@
 """Shared data models for all Niibot backend services."""
 
-from .analytics import CommandStat, StreamEvent, StreamSession
 from .birthday import Birthday, BirthdaySettings
 from .channel import Channel, DiscordUser, Token
 
@@ -8,9 +7,6 @@ __all__ = [
     "Birthday",
     "BirthdaySettings",
     "Channel",
-    "CommandStat",
     "DiscordUser",
-    "StreamEvent",
-    "StreamSession",
     "Token",
 ]
